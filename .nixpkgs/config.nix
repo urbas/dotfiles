3ac4c8f00aa66637306a821c5563cbd9f2a98ef1
Matej Urbas <matej.urbas@gmail.com>
2018-10-30 19:41:33 +0000

    installed ripgrep

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -4,13 +4,13 @@
     homeEnv = pkgs.buildEnv {
       name = "homeEnv";
       paths = [
-        pkgs.ag
         pkgs.bat
         pkgs.fd
         pkgs.fzf
         pkgs.gitAndTools.diff-so-fancy
         pkgs.htop
         pkgs.ncdu
+        pkgs.ripgrep
         pkgs.tldr
         pkgs.tmux
       ];
