b787209276e8125e21ab878d4589083f3cc54811
Matej Urbas <matej.urbas@gmail.com>
2019-09-04 14:05:55 +0100

    Adds nix to the homeEnv

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,26 +1,27 @@
 {
   packageOverrides = pkgs:
-  rec {
-    homeEnv = pkgs.buildEnv {
-      name = "homeEnv";
-      paths = [
-        pkgs.bat
-        pkgs.fd
-        pkgs.fzf
-        pkgs.gitAndTools.gitFull
-        pkgs.gitAndTools.diff-so-fancy
-        pkgs.go
-        pkgs.glibcLocales
-        pkgs.htop
-        pkgs.jq
-        pkgs.ncdu
-        pkgs.ripgrep
-        pkgs.tldr
-        pkgs.tmux
-        pkgs.travis
-        pkgs.xclip
-        pkgs.yq
-      ];
+    rec {
+      homeEnv = pkgs.buildEnv {
+        name = "homeEnv";
+        paths = [
+          pkgs.bat
+          pkgs.fd
+          pkgs.fzf
+          pkgs.gitAndTools.diff-so-fancy
+          pkgs.gitAndTools.gitFull
+          pkgs.glibcLocales
+          pkgs.go
+          pkgs.htop
+          pkgs.jq
+          pkgs.ncdu
+          pkgs.nix
+          pkgs.ripgrep
+          pkgs.tldr
+          pkgs.tmux
+          pkgs.travis
+          pkgs.xclip
+          pkgs.yq
+        ];
+      };
     };
-  };
 }
