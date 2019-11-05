7d219e372d9c7541fe758865e67f8adb7fdc739e
Matej Urbas <matej.urbas@gmail.com>
2019-11-05 22:26:43 +0000

    Disable tests on armv7l

--- .nixpkgs/matej.nix
+++ .nixpkgs/matej.nix
@@ -1,6 +1,6 @@
 let
   nixpkgsSrc = builtins.fetchTarball {
-    url = "https://github.com/urbas/nixpkgs/archive/09d24d12bb70c75206c0c99222d02186f9ce7539.tar.gz";
+    url = "https://github.com/urbas/nixpkgs/archive/32b7613bf9aaa2346fa355b5fcda66d6c325b511.tar.gz";
   };
 in rec {
   nixpkgs = (import "${nixpkgsSrc}") {};
