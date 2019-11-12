0d51c7526b222ad6577c20821a09424eeb407ba2
Matej Urbas <matej.urbas@gmail.com>
2019-11-12 08:35:20 +0000

    Moves .nixpkgs/matej.nix to .nixpkgs/raspbian.nix

--- .nixpkgs/raspbian.nix
+++ .nixpkgs/raspbian.nix
@@ -1,3 +1,5 @@
+# This expression uses a revision of nixpkgs that can build homeEnv on a Raspberry Pi running Raspbian
+# To build this, run the following: nix-env -f $HOME/.nixpkgs/raspbian.nix -vi homeEnv
 let
   nixpkgsSrc = builtins.fetchTarball {
     url = "https://github.com/urbas/nixpkgs/archive/51814d1de00d604bba71296de4d5aeb19cdcfc6e.tar.gz";
