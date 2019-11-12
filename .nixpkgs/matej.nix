e72122c2e05a00464a01ba4d05b8c01f5517cbef
Matej Urbas <matej.urbas@gmail.com>
2019-11-12 08:18:32 +0000

    Reverting back to older nixpkgs because in the new one rustc didn't build

--- .nixpkgs/matej.nix
+++ .nixpkgs/matej.nix
@@ -1,6 +1,6 @@
 let
   nixpkgsSrc = builtins.fetchTarball {
-    url = "https://github.com/urbas/nixpkgs/archive/d81a65468059169536db7b27dca1fac0659c03e8.tar.gz";
+    url = "https://github.com/urbas/nixpkgs/archive/51814d1de00d604bba71296de4d5aeb19cdcfc6e.tar.gz";
   };
 in rec {
   nixpkgs = (import "${nixpkgsSrc}") {};
