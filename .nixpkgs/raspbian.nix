51bb4b3a14ff5005c5734a3875d2602d1a83a370
Matej Urbas <matej.urbas@gmail.com>
2019-12-15 12:06:28 +0000

    updated instructions on how to build nix homeEnv on raspbian

--- .nixpkgs/raspbian.nix
+++ .nixpkgs/raspbian.nix
@@ -2,6 +2,7 @@
 # To build this, run the following: nix-env -f $HOME/.nixpkgs/raspbian.nix -vi homeEnv
 let
   nixpkgsSrc = builtins.fetchTarball {
+    # This rev comes from branch `raspbian-support`: https://github.com/urbas/nixpkgs/tree/raspbian-support
     url = "https://github.com/urbas/nixpkgs/archive/51814d1de00d604bba71296de4d5aeb19cdcfc6e.tar.gz";
   };
 in rec {
