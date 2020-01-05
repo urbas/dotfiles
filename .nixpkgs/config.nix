4209714dd8b6c23167efb4cf9df4d3c328fb17af
Matej Urbas <matej.urbas@gmail.com>
2020-01-05 16:14:36 +0000

    homeEnv

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -7,14 +7,20 @@
       philips-air-purifier = pythonPackages.buildPythonPackage (with pythonPackages; rec {
         pname = "philips-air-purifier";
         version = "0.0.4";
+        preConfigure = ''
+
+        '';
         checkPhase = ''
           pytest tests
         '';
         checkInputs = [ pytest pytestrunner pytestcov ];
         propagatedBuildInputs = [ pycryptodome requests ];
-        src = fetchPypi {
+        src = fetchGithub {
           inherit pname version;
-          sha256 = "0c2l9k1fk2by5nkgjx7g3h96d9b6y2nf1z01aygba68bl8i9cvx9";
+          owner = "urbas";
+          repo = pname;
+          rev = "74c0189284d3fbd8d345db401241ef5f759e4003";
+          sha256 = "000l9k1fk2by5nkgjx7g3h96d9b6y2nf1z01aygba68bl8i9cvx9";
         };
       });
 
