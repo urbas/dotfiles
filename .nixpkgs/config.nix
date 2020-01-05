5755e3bffae141cb6672f700a37b84548e9f0237
Matej Urbas <matej.urbas@gmail.com>
2020-01-05 15:00:28 +0000

    disable check for philips-air-purifier

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -8,6 +8,7 @@
           inherit pname version;
           sha256 = "0c2l9k1fk2by5nkgjx7g3h96d9b6y2nf1z01aygba68bl8i9cvx9";
         };
+        doCheck = false;
       };
 
       py = pkgs.python3.withPackages(ps: with ps; [
