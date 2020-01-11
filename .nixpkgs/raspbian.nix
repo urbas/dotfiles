0a79383beb79ca2c58302f98c6a19f0ad6b7e1ea
Matej Urbas <matej.urbas@gmail.com>
2020-01-11 13:10:24 +0000

    using unstable branch instead of master

--- .nixpkgs/raspbian.nix
+++ .nixpkgs/raspbian.nix
@@ -2,8 +2,8 @@
 # To build this, run the following: nix-env -f $HOME/.nixpkgs/raspbian.nix -vi homeEnv
 let
   nixpkgsSrc = builtins.fetchTarball {
-    # This rev comes from branch `raspbian-support`: https://github.com/urbas/nixpkgs/tree/raspbian-support
-    url = "https://github.com/urbas/nixpkgs/archive/4bb0f6c84a61215d141c04e4369f6ad1185582d8.tar.gz";
+    # This rev comes from branch `unstable`: https://github.com/urbas/nixpkgs/tree/unstable
+    url = "https://github.com/urbas/nixpkgs/archive/ef16a30c79c1dbb59235fbc049f4c136091610e2.tar.gz";
   };
 in rec {
   nixpkgs = (import "${nixpkgsSrc}") {};
