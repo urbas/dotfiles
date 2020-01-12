6c86fcf12908f97b2c908c350cbce32e5eb8f6a2
Matej Urbas <matej.urbas@gmail.com>
2020-01-12 12:14:35 +0000

    unstable nixpkgs

--- .nixpkgs/raspbian.nix
+++ .nixpkgs/raspbian.nix
@@ -3,7 +3,7 @@
 let
   nixpkgsSrc = builtins.fetchTarball {
     # This rev comes from branch `unstable`: https://github.com/urbas/nixpkgs/tree/unstable
-    url = "https://github.com/urbas/nixpkgs/archive/ef16a30c79c1dbb59235fbc049f4c136091610e2.tar.gz";
+    url = "https://github.com/urbas/nixpkgs/archive/39a340714b443cf7b7518711bff2b2f031a41ea1.tar.gz";
   };
 in rec {
   nixpkgs = (import "${nixpkgsSrc}") {};
