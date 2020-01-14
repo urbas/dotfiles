b8f3a88e03816b90a2858f7f15555de5973fd6e3
Matej Urbas <matej.urbas@gmail.com>
2020-01-14 09:29:00 +0000

    nix: added a comment for unpatched nixpkgs unstable

--- .nixpkgs/raspbian.nix
+++ .nixpkgs/raspbian.nix
@@ -5,6 +5,8 @@ let
     # This rev comes from branch `unstable` (based on upstream `nixpkgs-unstable` with some patches for
     # raspbian and Ubuntu x86_64): https://github.com/urbas/nixpkgs/tree/unstable
     url = "https://github.com/urbas/nixpkgs/archive/53b2ee842f90ebe816d8a018b6e6bfa6d33a66e9.tar.gz";
+    # Unstable nixpkgs:
+    # url = "https://github.com/NixOs/nixpkgs/archive/f9c81b5c148572c2a78a8c1d2c8d5d40e642b31a.tar.gz";
   };
 in rec {
   nixpkgs = (import "${nixpkgsSrc}") {};
