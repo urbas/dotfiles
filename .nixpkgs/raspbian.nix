62e1fa037c9a902cbb3d849e9c2e6881dc2c90b5
Matej Urbas <matej.urbas@gmail.com>
2020-01-15 21:57:39 +0000

    nix homeEnv: nixpkgs-unstable

--- .nixpkgs/raspbian.nix
+++ .nixpkgs/raspbian.nix
@@ -4,9 +4,9 @@ let
   nixpkgsSrc = builtins.fetchTarball {
     # This rev comes from branch `unstable` (based on upstream `nixpkgs-unstable` with some patches for
     # raspbian and Ubuntu x86_64): https://github.com/urbas/nixpkgs/tree/unstable
-    url = "https://github.com/urbas/nixpkgs/archive/53b2ee842f90ebe816d8a018b6e6bfa6d33a66e9.tar.gz";
+    # url = "https://github.com/urbas/nixpkgs/archive/53b2ee842f90ebe816d8a018b6e6bfa6d33a66e9.tar.gz";
     # Unstable nixpkgs:
-    # url = "https://github.com/NixOs/nixpkgs/archive/f9c81b5c148572c2a78a8c1d2c8d5d40e642b31a.tar.gz";
+    url = "https://github.com/NixOs/nixpkgs-channels/archive/7184df6beb88c4f5f3186e5b73d5437a3461ceaf.tar.gz";
   };
 in rec {
   nixpkgs = (import "${nixpkgsSrc}") {};
