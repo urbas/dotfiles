ae596187fb5d0bdbffe969de75e72cd749a6ef78
Matej Urbas <matej.urbas@gmail.com>
2020-01-16 19:03:24 +0000

    nix: fixed philips air purifier version

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -6,14 +6,13 @@
 
       philips-air-purifier = pythonPackages.buildPythonPackage (with pythonPackages; rec {
         name = "philips-air-purifier";
-        version = "0.0.5";
+        version = "0.1.0";
         checkInputs = [ httmock pytest pytestrunner pytestcov ];
         propagatedBuildInputs = [ pycryptodome requests ];
-        src = pkgs.fetchFromGitHub {
-          owner = "urbas";
-          repo = name;
-          rev = "v${version}";
-          sha256 = "07hrb1p8m46cd5kmxxjz5if1nx8d5h65y3v7px5zpsizli0hdfn4";
+        src = pkgs.fetchgit {
+          url = "https://github.com/urbas/philips-air-purifier";
+          rev = "687ab3a9a937cc2c672fb6e8059ddac2f55f43b6";
+          sha256 = "07hrb1p8m46cd5kmxxjz50000x8d5h65y3v7000zpsizli0hdfn4";
         };
       });
 
