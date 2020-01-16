06603d5a8f2a1960592b6e5ea91eac77a5ea56ce
Matej Urbas <matej.urbas@gmail.com>
2020-01-16 08:23:40 +0000

    nix: raspbian doesn't compile still

--- .nixpkgs/raspbian.nix
+++ .nixpkgs/raspbian.nix
@@ -4,9 +4,9 @@ let
   nixpkgsSrc = builtins.fetchTarball {
     # This rev comes from branch `unstable` (based on upstream `nixpkgs-unstable` with some patches for
     # raspbian and Ubuntu x86_64): https://github.com/urbas/nixpkgs/tree/unstable
-    # url = "https://github.com/urbas/nixpkgs/archive/53b2ee842f90ebe816d8a018b6e6bfa6d33a66e9.tar.gz";
+    url = "https://github.com/urbas/nixpkgs/archive/93f442dd7a7aaf31bc13a2d077b371fd33dce879.tar.gz";
     # Unstable nixpkgs:
-    url = "https://github.com/NixOs/nixpkgs-channels/archive/7184df6beb88c4f5f3186e5b73d5437a3461ceaf.tar.gz";
+    # url = "https://github.com/NixOs/nixpkgs-channels/archive/7184df6beb88c4f5f3186e5b73d5437a3461ceaf.tar.gz";
   };
 in rec {
   nixpkgs = (import "${nixpkgsSrc}") {};
