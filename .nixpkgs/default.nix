4a2ca8b1bd42359fe55c8719d754c8b8963f6e42
Matej Urbas <matej.urbas@gmail.com>
2020-02-01 23:11:02 +0000

    nix: 2020-02-01

--- .nixpkgs/default.nix
+++ .nixpkgs/default.nix
@@ -5,7 +5,7 @@ let
   nixpkgsSrc = builtins.fetchTarball {
     # This rev comes from branch `unstable` (based on upstream `nixpkgs-unstable` with some patches for
     # raspbian and Ubuntu x86_64): https://github.com/urbas/nixpkgs/tree/unstable
-    url = "https://github.com/urbas/nixpkgs/archive/4f181412757c03c771a2342e4dc3ab895b58b3fe.tar.gz";
+    url = "https://github.com/urbas/nixpkgs/archive/urbas-2020-02-01.tar.gz";
     # Unstable nixpkgs:
     # url = "https://github.com/NixOs/nixpkgs-channels/archive/7184df6beb88c4f5f3186e5b73d5437a3461ceaf.tar.gz";
   };
