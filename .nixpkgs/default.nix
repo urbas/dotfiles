73312cd99944d4bb5f6b60e89633fde166f611ef
Matej Urbas <matej.urbas@gmail.com>
2020-02-04 09:42:57 +0000

    nix: updated comments

--- .nixpkgs/default.nix
+++ .nixpkgs/default.nix
@@ -1,14 +1,11 @@
-# This expression uses a revision of nixpkgs that can build homeEnv on a Raspberry Pi running Raspbian
-# To build this, run the following: nix-env -f $HOME/.nixpkgs/homeEnv.nix -vi homeEnv
+# This expression uses a revision of nixpkgs that can build on a Raspberry Pi running Raspbian
 let
   # nixpkgsSrc = /home/mat/programming/nixpkgs;
   nixpkgsSrc = builtins.fetchTarball {
-    # This rev comes from branch `unstable` (based on upstream `nixpkgs-unstable` with some patches for
+    # This rev comes from branch `unstable` (based on upstream `nixpkgs-unstable` channel with some patches for
     # raspbian and Ubuntu x86_64): https://github.com/urbas/nixpkgs/tree/unstable
     url = "https://github.com/urbas/nixpkgs/archive/urbas-2020-02-01.tar.gz";
     sha256 = "173lx16cxabcxqhha82x9g2irzpx7hhbi3425q994zl0gwm9x6kf";
-    # Unstable nixpkgs:
-    # url = "https://github.com/NixOs/nixpkgs-channels/archive/7184df6beb88c4f5f3186e5b73d5437a3461ceaf.tar.gz";
   };
   
   inherit (import "${nixpkgsSrc}" {}) pkgs;
