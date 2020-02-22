ec7d2b7ece1b5a35764344795433e5e5c099da49
Matej Urbas <matej.urbas@gmail.com>
2020-02-22 09:43:41 +0000

    nix: updated

--- .nixpkgs/default.nix
+++ .nixpkgs/default.nix
@@ -1,12 +1,16 @@
 # This expression uses a revision of nixpkgs that can build on a Raspberry Pi running Raspbian
+{
+  nixpkgsVersion ? "urbas-2020-02-21",
+  nixpkgsSha ? "0vcy798198gnbwmj8qmj2a210d00qfci7fpwv3db134jcjdxwvqm"
+}:
 let
   # nixpkgsSrc = /home/mat/programming/nixpkgs;
   nixpkgsSrc = builtins.fetchTarball {
     # This rev comes from branch `unstable` (based on upstream `nixpkgs-unstable` channel with some patches for
     # Raspbian): https://github.com/urbas/nixpkgs/tree/unstable
     # Check for updates at: https://github.com/NixOS/nixpkgs-channels/commits/nixpkgs-unstable
-    url = "https://github.com/urbas/nixpkgs/archive/urbas-2020-02-21.tar.gz";
-    sha256 = "1rfnqhkcyrdazi9l74i25ha74wc9r674n1s5acnsg59bas60yxwx";
+    url = "https://github.com/urbas/nixpkgs/archive/${nixpkgsVersion}.tar.gz";
+    sha256 = nixpkgsSha;
   };
 
   inherit (import "${nixpkgsSrc}" {}) pkgs;
