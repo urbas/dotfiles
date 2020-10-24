56fa30d031351420febc1a7624b6e05aec2166f7
Matej Urbas <matej.urbas@gmail.com>
2020-10-24 21:16:24 +0100

    nix: formatted home env

--- .nixpkgs/envs/home.nix
+++ .nixpkgs/envs/home.nix
@@ -5,31 +5,30 @@ let
     (pkgs.callPackage ../python/philips-air-purifier.nix {})
     youtube-dl
   ]);
-in
-  pkgs.buildEnv {
-    name = "home";
-    paths = with pkgs; [
-      bat
-      direnv
-      docker-compose
-      entr
-      fd
-      fzf
-      gitAndTools.diff-so-fancy
-      gitAndTools.gitFull
-      glibcLocales
-      htop
-      hyperfine
-      jq
-      ncdu
-      nix
-      py
-      ripgrep
-      starship
-      tldr
-      tmux
-      travis
-      xclip
-      yq
-    ];
-  }
+in pkgs.buildEnv {
+  name = "home";
+  paths = with pkgs; [
+    bat
+    direnv
+    docker-compose
+    entr
+    fd
+    fzf
+    gitAndTools.diff-so-fancy
+    gitAndTools.gitFull
+    glibcLocales
+    htop
+    hyperfine
+    jq
+    ncdu
+    nix
+    py
+    ripgrep
+    starship
+    tldr
+    tmux
+    travis
+    xclip
+    yq
+  ];
+}
