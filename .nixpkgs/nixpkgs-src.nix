4f3da4c46a45e23724fde963a8112a4f11452683
Matej Urbas <matej.urbas@gmail.com>
2021-08-05 21:06:35 +0100

    nixpkgs-unstable update

--- .nixpkgs/nixpkgs-src.nix
+++ .nixpkgs/nixpkgs-src.nix
@@ -1,6 +1,6 @@
 builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/141b2b111d7aa5f8132edd0fc4f78fb2c2402ebb.tar.gz"; # urbas-nixpkgs
-  sha256 = "sha256:1fra46x7pwcxr6s1ihjl94afjlpv3k0dmcy28j76vlflp9fw0hk6";
+  url = "https://github.com/NixOS/nixpkgs/archive/6dc0549183672acd39d735a465098ca9deeff6ad.tar.gz";
+  sha256 = "sha256:1dzhxs6fpaabfrw75xhpdh4j4q7fr21686kl1ag70m4rgf6m6h3h";
 }
 # builtins.fetchGit {
 #   url = "/home/mat/programming/nixpkgs";
