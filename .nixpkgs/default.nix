028a252157e4dd831978dbeca32634b1f37ab465
Matej Urbas <matej.urbas@gmail.com>
2022-02-19 09:16:16 +0000

    remove unused env

--- .nixpkgs/default.nix
+++ .nixpkgs/default.nix
@@ -6,13 +6,12 @@ let
     (import ./overlays/1-pkgs.nix)
   ];
 
-  inherit (import "${nixpkgs-src}" { inherit overlays; }) pkgs;
+  inherit (import nixpkgs-src { inherit overlays; }) pkgs;
 in {
   inherit pkgs;
 
   envs = {
     home = import ./envs/home.nix { inherit pkgs; };
-    nixos-config-dev = import ./envs/nixos-config-dev.nix { inherit pkgs; };
     nixpkgs-src = pkgs.buildEnv { name = "nixpkgs-src"; paths = [ nixpkgs-src ]; };
   };
 }
