a27fb2674c9d1aa1d6849fa7c04cdef91afcd356
Matej Urbas <matej.urbas@gmail.com>
2022-07-30 08:50:44 +0100

    nixpkgs update

--- .nixpkgs/nixpkgs-src.nix
+++ .nixpkgs/nixpkgs-src.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/83658b28fe638a170a19b8933aa008b30640fbd1.tar.gz";
-  sha256 = "sha256:11knahk5j90aaxskh204dbg4px5mgrp7pbgm77g6ck2cbv8imyaj";
+  url = "https://github.com/NixOS/nixpkgs/archive/2a93ea177c3d7700b934bf95adfe00c435f696b8.tar.gz";
+  sha256 = "sha256:0zyqnnywry5llsssrrvxzxx1idxw9lvdir4ap24abjvd2lfchi9z";
 }
