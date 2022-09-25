0534dcb69cb857bb1dff9d1f56caff4a1c2485b0
Matej Urbas <matej.urbas@gmail.com>
2022-09-25 09:02:06 +0100

    nixpkgs bump

--- .nixpkgs/nixpkgs-src.nix
+++ .nixpkgs/nixpkgs-src.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/2a93ea177c3d7700b934bf95adfe00c435f696b8.tar.gz";
-  sha256 = "sha256:0zyqnnywry5llsssrrvxzxx1idxw9lvdir4ap24abjvd2lfchi9z";
+  url = "https://github.com/NixOS/nixpkgs/archive/fde244a8c7655bc28616864e2290ad9c95409c2c.tar.gz";
+  sha256 = "sha256:07mmm1kcbw96jggfzv8vyi471f0kazifl2ijjd9mgi2kl1j5cpzp";
 }
