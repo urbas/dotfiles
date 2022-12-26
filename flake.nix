881609b018be1c3f658c6701094a8b5db8827ef2
Matej Urbas <matej.urbas@gmail.com>
2022-12-26 09:09:16 +0000

    flakes

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forSupportedSystems = f: with nixpkgs.lib; foldl' (resultAttrset: system: recursiveUpdate resultAttrset (f { inherit system; pkgs = import nixpkgs { inherit system; }; })) {} supportedSystems;
+
+    in forSupportedSystems ({ pkgs, system, ... }:
+      let
+        homeEnv = import ./.nixpkgs/envs/home.nix { inherit pkgs; };
+        mkInputSourceCache = input: pkgs.runCommand "${baseNameOf (toString input)}" { } "mkdir $out && ln -s ${input} $out/$(basename ${input})";
+        input-sources = map mkInputSourceCache [nixpkgs];
+
+      in {
+        devShells.${system}.default = pkgs.stdenvNoCC.mkDerivation { name = "dotfiles"; buildInputs = homeEnv.paths ++ input-sources; };
+        packages.${system}.homeEnv = homeEnv;
+      }
+    );
+}
