3a8d92038e97b2827ef5b403e77bf8911df10deb
Matej Urbas <matej.urbas@gmail.com>
2023-03-28 19:02:30 +0100

    only flakes

--- .nixpkgs/nixpkgs-src.nix
+++
@@ -1,4 +0,0 @@
-builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/6dccdc458512abce8d19f74195bb20fdb067df50.tar.gz";
-  sha256 = "sha256:1is1icy39vvij73zv74qxc644agrgwpfvn5375k974ifx7s64inn";
-}
