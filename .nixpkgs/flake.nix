a574622a6f000d13adad57b3f35f3f7ff12aa687
Matej Urbas <matej.urbas@gmail.com>
2023-08-27 09:33:57 +0100

    flake.nix simplified

--- .nixpkgs/flake.nix
+++ .nixpkgs/flake.nix
@@ -1,23 +1,11 @@
 {
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forSupportedSystems = f: with nixpkgs.lib; foldl' (resultAttrset: system: recursiveUpdate resultAttrset (f { inherit system; pkgs = import nixpkgs { inherit system; }; })) {} supportedSystems;
-
-    in forSupportedSystems ({ pkgs, system, ... }:
-      let
-        homeEnv = import ./envs/home.nix { inherit pkgs; };
-        mkInputSourceCache = input: pkgs.runCommand "${baseNameOf (toString input)}" { } "mkdir $out && ln -s ${input} $out/$(basename ${input})";
-        input-sources = map mkInputSourceCache [nixpkgs];
-
-      in {
-        devShells.${system}.default = pkgs.stdenvNoCC.mkDerivation { name = "dotfiles"; buildInputs = homeEnv.paths ++ input-sources; };
-        packages.${system} = {
-          inherit homeEnv pkgs;
-          default = homeEnv;
-        };
-      }
-    );
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+    with nixpkgs.legacyPackages.${system}; rec {
+      devShells.default = mkShell { buildInputs = packages.default.paths; };
+      packages.default = import ./envs/home.nix { inherit pkgs; };
+      packages.pkgs = pkgs;
+    });
 }
