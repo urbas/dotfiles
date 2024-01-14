c2ce5c860a30ffcc38e95c1feb24b4488fbf1b06
Matej Urbas <matej.urbas@gmail.com>
2024-01-14 13:11:26 +0000

    using nixos-23.11 for dependencies

--- .nixpkgs/flake.nix
+++ .nixpkgs/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "nixpkgs/nixos-23.11";
 
   outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
     with (import nixpkgs { inherit system; config.allowUnfree = true; }); rec {
