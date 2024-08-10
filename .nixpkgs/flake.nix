8f3c1628115386d036b3b4717678ea8153d18910
Matej Urbas <matej.urbas@gmail.com>
2024-08-10 11:05:39 +0100

    nixos 24.05

--- .nixpkgs/flake.nix
+++ .nixpkgs/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
 
   outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
     with (import nixpkgs { inherit system; config.allowUnfree = true; }); let
